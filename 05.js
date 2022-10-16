7fa7b48e1271a80f01219a4592c97759caba4ef7
maricover <you@example.com>
2022-10-16 12:08:57 -0500

    Prueba Challenge

--- 05.js
+++ 05.js
@@ -14,6 +14,10 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
+  var nuevoArreglo = array.map(function(x){
+    return x[propiedad];
+  })
+  return nuevoArreglo;
 
 }
 
